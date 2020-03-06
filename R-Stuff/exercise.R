e5cf23146d7a1f781015cf7f1078bbb16e8e10e0
Rastatrevftw1538 <trevor.cardoza1538@gmail.com>
2020-03-05 18:01:19 -0800

    new stuff

---
+++ R-Stuff/exercise.R
@@ -0,0 +1,5 @@
+num.vector = c(1.2,5,7,10,3.5)
+char.vector = c('one point two', 'five, seven', 'ten', 'three point five')
+char2.vector = c("small number", "small number", "big number","big number", "small number")
+numbers = data.frame(num.vector,char.vector,char2.vector)
+numbers
